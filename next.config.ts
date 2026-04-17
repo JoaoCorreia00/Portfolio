f4fa2b126a3c3ec6b4ffc1da2ec872725910ae4a
João Correia <jotagamertv@gmail.com>
2026-04-17 14:36:25 +0100

    Trying to fix name basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/portfolio",
+  basePath: "/Portfolio",
   output: "export",  // <== enables static exports
   reactStrictMode: true,
 };
